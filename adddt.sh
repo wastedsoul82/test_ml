df94efb64e6b0cdea54d0538e912b0ebab63db7c
root <root@ip-172-31-49-113.ec2.internal>
2021-03-11 11:59:24 +0000

    12th 1000

--- adddt.sh
+++ adddt.sh
@@ -1,6 +1,7 @@
+dvc push
 dvc run -n new1  --force python3 src/catname.py
 git add dvc.yaml
 dvc add csvdata/name.csv
 git add csvdata/name.csv.dvc
-hdfs dfs -copyFromLocal -f  csvdata/name.csv /user/hive/
+hdfs dfs -copyFromLocal -f  csvdata/name.csv /user/hive/data/
 
--- chkout.sh
+++ chkout.sh
@@ -5,5 +5,5 @@ dvc run -n new1  --force python3 src/catname.py
 git add dvc.yaml
 dvc add csvdata/name.csv
 git add csvdata/name.csv.dvc
-hdfs dfs -copyFromLocal csvdata/name.csv /user/hive/
+hdfs dfs -copyFromLocal -f csvdata/name.csv /user/hive/data/
 
